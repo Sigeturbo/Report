61b01ca2aa18582c31c46bfbabb9c81176f50840
José Jaraba <jijaraba@hotmail.com>
2016-07-14 13:18:43 -0500

    MimeTypes

--- src/GenerateReport.php
+++ src/GenerateReport.php
@@ -67,14 +67,14 @@ class GenerateReport
      */
     private function prepareForDownload($data, $filename, $format)
     {
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        header('Content-Description: File Transfer');
+        header('Cache-Control: max-age=0');
+        //header('Pragma: public');
+        //header('Content-Description: File Transfer');
         header('Content-Disposition: attachment;filename="' . $filename . '.' . $format . '"');
         //header('Content-Transfer-Encoding: binary');
-        header('Content-Length: ' . strlen($data));
+        //header('Content-Length: ' . strlen($data));
         if (isset($this->mime_types[$format])) {
-            header('Content-Type: ' . $this->mime_types[$format]);
+            //header('Content-Type: ' . $this->mime_types[$format]);
         } else {
             header('Content-Type: application/octet-stream');
         }
