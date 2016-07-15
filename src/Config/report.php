30ca465e488f16e4554bbffd276daf0c0b05cefb
José Jaraba <jijaraba@hotmail.com>
2016-07-15 12:49:37 -0500

    MimeTypes

--- src/Config/report.php
+++ src/Config/report.php
@@ -3,4 +3,5 @@ return [
     'server' => 'http://23.253.151.166',
     'username' => 'jasperadmin',
     'password' => '*jija_01*08',
+    'organization' => null,
 ];
--- src/GenerateReport.php
+++ src/GenerateReport.php
@@ -12,20 +12,31 @@ class GenerateReport
     private $username;
     private $password;
     private $organization;
+    private $storage;
+    private $mime_types = array(
+        'html' => 'text/html',
+        'pdf' => 'application/pdf',
+        'xls' => 'application/vnd.ms-excel',
+        'csv' => 'text/csv',
+        'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        'rtf' => 'text/rtf',
+        'odt' => 'application/vnd.oasis.opendocument.text',
+        'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
+        'xlsx' => 'application/xlsx'
+    );
+
 
     /**
      * Generate constructor.
-     * @param $username
-     * @param $password
-     * @param $organization
-     * @internal param $client
+     * @param $storage
      */
-    public function __construct($username, $password, $organization)
+    public function __construct($storage)
     {
         $this->server = config('report.message');
         $this->username = config('report.username');
         $this->password = config('report.password');
         $this->organization = config('report.organization');
+        $this->storage = $storage;
     }
 
     /**
@@ -37,17 +48,27 @@ class GenerateReport
      */
     public function run($uri, $format = 'pdf', $filename = 'report', $controls = [])
     {
-        $this->client = new Client();
-        $myFile = fopen(storage_path() . '/sigeturbo/report/report1.xlsx',"w") or die("Problems");
-        $request = new Request('GET','http://23.253.151.166:8080/jasperserver/rest_v2/reports/reports/sigeturbo/Purchases/Purchase.xlsx?interactive=true&onePagePerSheet=false&freshData=true&saveDataSnapshot=false&codeID=20160715-01');
-        $this->client->send($request, [
-            'auth' => [$this->username, $this->password],
-            'alt' => 'media',
-            'sink'=>$myFile
-        ]);
+        if ($this->mimeType($format)) {
+            $this->client = new Client();
+            $myFile = fopen($this->storage . '/' . $filename . "." . $format, "w") or die("Problems");
+            $request = new Request('GET', 'http://23.253.151.166:8080/jasperserver/rest_v2/reports/reports/sigeturbo/Purchases/Purchase.xlsx?interactive=true&onePagePerSheet=false&freshData=true&saveDataSnapshot=false&codeID=20160715-01');
+            $this->client->send($request, [
+                'auth' => [$this->username, $this->password],
+                'alt' => 'media',
+                'sink' => $myFile
+            ]);
+        }
 
     }
 
+
+    private function mimeType($format)
+    {
+        if ($this->mime_types[$format]) {
+            return true;
+        }
+        return false;
+    }
 
 
 }
